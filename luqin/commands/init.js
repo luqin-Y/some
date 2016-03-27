7f6ae938f52d4bb652985925498c0cc7944232af
= <st199@study.com>
2016-03-27 22:30:34 +0800

    整理init结构

--- luqin/commands/init.js
+++ luqin/commands/init.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const fs = require('fs');
@@ -6,30 +5,16 @@ const path = require('path');
 const config = require('../config.js');
 
 const stru = config.structure;
-const rootName= './'+stru.name;
-// C:\Users\Administrator
-module.exports=function(){
-  // if (stru.name) {
-  //   fs.mkdir(rootName, function () {
-  //     // 遍历 fileData ，如果是目录，创建目录，如果是文件，创建文件
-  //     stru.fileData.forEach(function (item) {
-  //       if (item.type === 'dir') {
-  //         fs.mkdir(rootName + '/' + item.name);
-  //       } else if (item.type === 'file') {
-  //         fs.writeFile(rootName + '/' + item.name, item.content);
-  //       }
-  //     });
-  //   });
-  //   console.log(2222);
-  // }
+const rootName = './' + stru.name;
+module.exports = function() {
 
-  if(stru.name){
+  if (stru.name) {
     fs.mkdir(rootName, () => {
-      stru.fileData.forEach(function (item){
-        if(item.type === 'dir'){
+      stru.fileData.forEach(function(item) {
+        if (item.type === 'dir') {
           // 创建目录
           fs.mkdir(rootName + '/' + item.name);
-        }else if(item.type === 'file'){
+        } else if (item.type === 'file') {
           // 创建文件 writeFile content
           fs.writeFile(rootName + '/' + item.name, item.content, 'utf-8');
         }
